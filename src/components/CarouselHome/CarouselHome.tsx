32e852dfe83de312ac490bddad1e17846fea519a
Teneze <ezequielmtenerini@gmail.com>
2023-11-21 20:10:15 -0300

    Cambios en Home

--- src/components/CarouselHome/CarouselHome.tsx
+++ src/components/CarouselHome/CarouselHome.tsx
@@ -3,40 +3,40 @@ import { Carousel } from "react-bootstrap"
 const CarouselHome = ()=>{
     return(
         <>
-            <Carousel>
-      <Carousel.Item>
-        <img
-        className='d-block w-100'
-        style={{maxHeight:"500px",objectFit:'cover'}}
-        src="images/slide1.jpg" alt="Imagen Hamburgesa" />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-        className='d-block w-100'
-        style={{maxHeight:"500px",objectFit:'cover'}}
-        src="images/slide1.jpg" alt="Imagen Hamburgesa" />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-      <img
-      className='d-block w-100'
-      style={{maxHeight:"500px",objectFit:'cover'}}
-      src="images/slide1.jpg" alt="Imagen Hamburgesa" />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className='d-block w-100'
+              style={{maxHeight:"500px",objectFit:'cover'}}
+              src="images/slide1.jpg" alt="Imagen Hamburgesa" />
+            <Carousel.Caption>
+              <h3>Hamburguesa Completa</h3>
+              <p>Disfruta de una experiencia con el mejor cheddar, bacon y cebolla.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className='d-block w-100'
+              style={{maxHeight:"500px",objectFit:'cover'}}
+              src="images/slide2.jpg" alt="Imagen Hamburgesa" />
+            <Carousel.Caption>
+              <h3>Hamburguesa Pepino</h3>
+              <p>Disfruta de una experiencia épica (solo para amantes del pepino).</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+            className='d-block w-100'
+            style={{maxHeight:"500px",objectFit:'cover'}}
+            src="images/slide3.jpg" alt="Imagen Hamburgesa" />
+              <Carousel.Caption>
+                <h3>Hamburguesa Bacon</h3>
+                <p>
+                Disfruta de una experiencia llena de bacon y cheddar.
+                </p>
+              </Carousel.Caption>
+          </Carousel.Item>
+          </Carousel>
         </>
     
     )
