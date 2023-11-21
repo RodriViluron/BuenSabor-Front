fb2eafd47ca5936edd24e3206a911f64613b2b77
andresnovello <andresnovello.03@gmail.com>
2023-11-21 18:02:11 -0300

    Commit #2

--- src/components/CarouselHome/CarouselHome.tsx
+++ src/components/CarouselHome/CarouselHome.tsx
@@ -8,7 +8,7 @@ const CarouselHome = ()=>{
         <img
         className='d-block w-100'
         style={{maxHeight:"500px",objectFit:'cover'}}
-        src="images/Hamburguesa5.jpg" alt="Imagen Hamburgesa" />
+        src="images/slide1.jpg" alt="Imagen Hamburgesa" />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
@@ -18,7 +18,7 @@ const CarouselHome = ()=>{
         <img
         className='d-block w-100'
         style={{maxHeight:"500px",objectFit:'cover'}}
-        src="images/Hamburguesa5.jpg" alt="Imagen Hamburgesa" />
+        src="images/slide1.jpg" alt="Imagen Hamburgesa" />
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -28,7 +28,7 @@ const CarouselHome = ()=>{
       <img
       className='d-block w-100'
       style={{maxHeight:"500px",objectFit:'cover'}}
-      src="images/Hamburguesa5.jpg" alt="Imagen Hamburgesa" />
+      src="images/slide1.jpg" alt="Imagen Hamburgesa" />
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
